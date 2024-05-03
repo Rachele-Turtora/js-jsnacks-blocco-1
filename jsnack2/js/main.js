812dd6de10caa72a878ef9c1c929185f0e5231ec
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-03 12:02:50 +0200

    [FIX]

--- jsnack2/js/main.js
+++ jsnack2/js/main.js
@@ -1,14 +1,14 @@
 "use strict";
 
-const parola1 = prompt("Scrivi una parola")
-const parola2 = prompt("Scrivi un'altra parola")
+const parola1 = prompt("Scrivi una parola");
+const parola2 = prompt("Scrivi un'altra parola");
 
 if (parola1.length > parola2.length) {
-    console.log(parola1)
-    console.log(parola2)
+    console.log(parola2);
+    console.log(parola1);
 } else if (parola1.length < parola2.length) {
-    console.log(parola2)
-    console.log(parola1)
+    console.log(parola1);
+    console.log(parola2);
 } else {
-    console.log("Le parole hanno la stessa lunghezza")
+    console.log("Le parole hanno la stessa lunghezza");
 }
