10e2182467341fd88094808dd9a86911b7c17e18
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-03 15:06:10 +0200

    [UPD] snack9

---
+++ jsnack9/README.md
@@ -0,0 +1,10 @@
+- definisco una variabile somma inizializzandola a 0
+- definisco un variabile numeroMassimo (10)
+
+- ciclo da 1 a numeroMassimo di volte:
+    - aggiungo l'indice alla somma
+
+- definisco una variabile media come somma/numeroMassimo
+
+- stampo la somma
+- stampo la media
--- jsnack9/js/main.js
+++ jsnack9/js/main.js
@@ -1,1 +1,16 @@
+/* Calcola la somma e la media dei primi 10 numeri.
+Stampa i risultati in console */
+
 "use strict";
+
+let somma = 0;
+const maxNum = 10;
+
+for (let i = 1; i <= maxNum; i++) {
+    somma += i;
+}
+
+const media = (somma / maxNum);
+
+console.log(`La somma dei primi ${maxNum} numeri è ${somma}`);
+console.log(`La media dei primi ${maxNum} numeri è ${media}`);
