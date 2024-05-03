b515c8c834b83690414403c74b56657dc79caf9e
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-03 11:46:20 +0200

    first commit

---
+++ jsnack2/README.md
@@ -0,0 +1,10 @@
+- Prendo due paole dall'utente con due prompt
+
+- se la prima parola è più lunga della secondda:
+    stampo prima parola
+    stampo seconda parola
+  se la seconda è più lunga della prima:
+    stampo la seconda
+    stampo la prima
+  altrimenti:
+    stampo messaggio  
---
+++ jsnack2/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const parola1 = prompt("Scrivi una parola")
+const parola2 = prompt("Scrivi un'altra parola")
+
+if (parola1.length > parola2.length) {
+    console.log(parola1)
+    console.log(parola2)
+} else if (parola1.length < parola2.length) {
+    console.log(parola2)
+    console.log(parola1)
+} else {
+    console.log("Le parole hanno la stessa lunghezza")
+}
